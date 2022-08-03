df99eeaf340092a895a2a55799b2b5df5fc4cfbb
Nazrin shah Fernandez <nfernand@u82z06s01.42kl.edu.my>
2022-08-03 17:10:29 +0800

    added lines to grid

--- src/cub3d/main.c
+++ src/cub3d/main.c
@@ -31,9 +31,32 @@ void	draw_square(t_data *data, int i, int j, int colour)
 		y = 0;
 		while (y < TILE_SIZE)
 		{
-			data->img.data[WIDTH * (y + j) + (x + i)] = colour;
+			data->img.data[WIDTH * (x + i) + (y + j)] = colour;
 			y++;
 		}
+		x++;
+	}
+}
+
+void	draw_grid(t_data *data, int i, int j, int colour)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < TILE_SIZE)
+	{
+		y = 0;
+		if (x == 0 || x == TILE_SIZE)
+		{
+			while (y < TILE_SIZE)
+			{
+				data->img.data[WIDTH * (i) + (y + j)] = colour;
+				y++;
+			}
+		}
+		else
+			data->img.data[WIDTH * (i + x) + (j)] = colour;
 		x++;
 	}
 }
@@ -43,21 +66,21 @@ void	draw_tiles(t_data *data)
 	int	i;
 	int	j;
 
-	i = 0;	
+	i = 0;
 	while (i < ROW)
 	{
 		j = 0;
 		while (j < COL)
 		{
 			if (data->map[i][j] == 1)
-				draw_square(data, TILE_SIZE * j, TILE_SIZE * i, 0xFF0000);
+				draw_square(data, TILE_SIZE * i, TILE_SIZE * j, 0xFF0000);
 			else
-				draw_square(data, TILE_SIZE * j, TILE_SIZE * i, 0xFFFFFF);
+				draw_square(data, TILE_SIZE * i, TILE_SIZE * j, 0xFFFFFF);
+			draw_grid(data, TILE_SIZE * i, TILE_SIZE * j, 0x808080);
 			j++;
 		}
 		i++;
 	}
-
 }
 
 int	draw_loop(t_data *data)
@@ -85,12 +108,16 @@ int	main()
 	};
 
 	data.mlx = mlx_init();
-	data.win = mlx_new_window(data.mlx, 800, 600, "cub3d");
+	data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "cub3d");
 	ft_memcpy(data.map, map, sizeof(int) * ROW * COL);
 
 	data.img.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
 	data.img.data = (int*)mlx_get_data_addr(data.img.img,
 			&data.img.bpp, &data.img.line_size, &data.img.endian);
+
+	printf("bpp = %d\n", data.img.bpp);
+	printf("line_size = %d\n", data.img.line_size);
+	printf("endian = %d\n", data.img.endian);
 
 	mlx_hook(data.win, X_KEY_PRESS, 0, handle_key, &data);
 	mlx_hook(data.win, X_KEY_EXIT, 0, handle_exit, &data);
