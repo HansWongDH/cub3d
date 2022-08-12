07d85de25896ca3ce0c134b56d0cb41ade8619e0
Nazrin shah Fernandez <nfernand@u82z06s01.42kl.edu.my>
2022-08-12 18:11:58 +0800

    bug somewhere

---
+++ inc/game_class.h
@@ -0,0 +1,29 @@
+#ifndef	GAME_CLASS_H
+# define GAME_CLASS_H
+
+# include <mlx.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include <float.h>
+
+# include "map_class.h"
+# include "macros.h"
+# include "structs.h"
+
+//typedef struct	c_game		t_game;
+//
+//typedef void	(*t_draw_sky)(t_game *self);
+//
+//struct		c_game
+//{
+//	t_draw_sky	draw_sky;
+//	t_img		img;
+//	int			height;
+//	int			width;
+//};
+//
+//t_game	game_init(t_map *map, void *mlx);
+//void	draw_sky(t_game *self);
+
+#endif
